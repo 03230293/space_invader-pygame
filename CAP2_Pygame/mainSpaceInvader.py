27838bf293ae023ff9c0cf413b80c74344f0b3f1
03230293 <03230293.gcbs@rub.edu.bt>
2023-11-17 23:31:43 +0600

    final

--- CAP2_Pygame/mainSpaceInvader.py
+++ CAP2_Pygame/mainSpaceInvader.py
@@ -1,136 +1,151 @@
-#https://youtu.be/FfWpgLFMI7w
-
+# Import modules
 import math
 import random
-
 import pygame
 from pygame import mixer
 
-# Intialize the pygame
+# Start pygame
 pygame.init()
 
-# create the screen
+# Make the screen
 screen = pygame.display.set_mode((800, 600))
 
-# Background
+# Load the background image
 background = pygame.image.load('background.png')
 
-# Sound
+# Load the background music
 mixer.music.load("background.wav")
 mixer.music.play(-1)
 
-# Caption and Icon
+# Set the title and icon of the window
 pygame.display.set_caption("Space Invader")
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
-# Player
+# Load the player image
 playerImg = pygame.image.load('player.png')
-playerX = 370
-playerY = 480
-playerX_change = 0
+playerX = 370 # Initial x-coordinate of the player
+playerY = 480 # Initial y-coordinate of the player
+playerX_change = 0 # Change in x-coordinate of the player
 
-# Enemy
+# Load the enemy images
 enemyImg = []
 enemyX = []
 enemyY = []
 enemyX_change = []
 enemyY_change = []
-num_of_enemies = 6
+num_of_enemies = 6 # Number of enemies
 
+# Create enemies with random positions and speeds
 for i in range(num_of_enemies):
-    enemyImg.append(pygame.image.load('enemy.png'))
-    enemyX.append(random.randint(0, 736))
-    enemyY.append(random.randint(50, 150))
-    enemyX_change.append(2)  #4
-    enemyY_change.append(20)  #40
+    enemyImg.append(pygame.image.load('ufo.png'))
+    enemyX.append(random.randint(0, 736)) # Random x-coordinate of the enemy
+    enemyY.append(random.randint(50, 150)) # Random y-coordinate of the enemy
+    enemyX_change.append(4)  # Speed of the enemy in x-direction
+    enemyY_change.append(40)  # Speed of the enemy in y-direction
 
-# Bullet
-
-# Ready - You can't see the bullet on the screen
-# Fire - The bullet is currently moving
-
+# Load the bullet image
 bulletImg = pygame.image.load('bullet.png')
-bulletX = 0
-bulletY = 480
-bulletX_change = 0
-bulletY_change = 10
-bullet_state = "ready"
+bulletX = 0 # Initial x-coordinate of the bullet
+bulletY = 480 # Initial y-coordinate of the bullet
+bulletX_change = 0 # Change in x-coordinate of the bullet
+bulletY_change = 10 # Speed of the bullet in y-direction
+bullet_state = "ready" # State of the bullet: ready or fire
 
-# Score
-
+# Initialize the score
 score_value = 0
-font = pygame.font.Font('freesansbold.ttf', 32)
+font = pygame.font.Font('freesansbold.ttf', 32) # Font for the score
 
-textX = 10
-testY = 10
+textX = 10 # x-coordinate of the score text
+textY = 10 # y-coordinate of the score text
 
-# Game Over
+# Load the game over font
 over_font = pygame.font.Font('freesansbold.ttf', 64)
 
 
 def show_score(x, y):
-    score = font.render("Score : " + str(score_value), True, (255, 255, 255))
+    # Render the score text
+    score = font.render("Score: " + str(score_value), True, (255, 255, 255))
+    # Display the score text on the screen
     screen.blit(score, (x, y))
 
 
 def game_over_text():
+    # Render the game over text
     over_text = over_font.render("GAME OVER", True, (255, 255, 255))
+    # Display the game over text on the screen
     screen.blit(over_text, (200, 250))
 
 
 def player(x, y):
+    # Display the player image on the screen
     screen.blit(playerImg, (x, y))
 
 
 def enemy(x, y, i):
+    # Display the enemy image on the screen
     screen.blit(enemyImg[i], (x, y))
 
 
 def fire_bullet(x, y):
     global bullet_state
-    bullet_state = "fire"
+    bullet_state = "fire" # Change the bullet state to fire
+    # Display the bullet image on the screen
     screen.blit(bulletImg, (x + 16, y + 10))
 
-def isCollision(enemyX, enemyY, bulletX, bulletY):
-    distance = math.sqrt(math.pow(enemyX - bulletX, 2) + (math.pow(enemyY - bulletY, 2)))
+
+def is_collision(enemyX, enemyY, bulletX, bulletY):
+    # Calculate the distance between the enemy and the bullet
+    distance = math.sqrt((enemyX - bulletX)**2 + (enemyY - bulletY)**2)
+    # Return True if the distance is less than 27 pixels, otherwise return False
     if distance < 27:
         return True
     else:
         return False
 
 
-# Game Loop
+# Main Loop
 running = True
 while running:
 
-    # RGB = Red, Green, Blue
+    # Set the screen color to black
     screen.fill((0, 0, 0))
-    # Background Image
+    # Display the background image on the screen
     screen.blit(background, (0, 0))
+    # Handle the events in the game
     for event in pygame.event.get():
+        # If the user clicks the close button, exit the game
         if event.type == pygame.QUIT:
             running = False
 
-        # if keystroke is pressed check whether its right or left
+        # Check if a key is pressed, and if it's left, right, or space
         if event.type == pygame.KEYDOWN:
+            # If the left arrow key is pressed, move the player to the left
             if event.key == pygame.K_LEFT:
                 playerX_change = -5
-            if event.key == pygame.K_RIGHT:
+            # If the right arrow key is pressed, move the player to the right
+            elif event.key == pygame.K_RIGHT:
                 playerX_change = 5
-            if event.key == pygame.K_SPACE:
-                if bullet_state is "ready":
+            # If the space key is pressed, shoot the bullet
+            elif event.key == pygame.K_SPACE:
+                # If the bullet is ready, play the laser sound and fire the bullet
+                if bullet_state == "ready":
                     bulletSound = mixer.Sound("laser.wav")
                     bulletSound.play()
-                    # Get the current x cordinate of the spaceship
+                    # Get the current x-coordinate of the spaceship
                     bulletX = playerX
                     fire_bullet(bulletX, bulletY)
 
+        # Stop player movement when a key is released
         if event.type == pygame.KEYUP:
+            # If the left or right arrow key is released, stop the player movement
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
 
+    # Change player position according to the player movement
     playerX += playerX_change
+
+    # Keep the player within the screen boundaries
     if playerX <= 0:
         playerX = 0
     elif playerX >= 736:
@@ -140,42 +155,54 @@ while running:
     for i in range(num_of_enemies):
 
         # Game Over
+        # If any enemy reaches the bottom of the screen, end the game
         if enemyY[i] > 440:
+            # Move all the enemies off the screen
             for j in range(num_of_enemies):
-                enemyY[j] = 2000 #2000
+                enemyY[j] = 2000
+            # Display the game over text
             game_over_text()
             break
 
+        # Change enemy position according to the enemy speed
         enemyX[i] += enemyX_change[i]
-        if enemyX[i] <= 0:
-            enemyX_change[i] = 4
-            enemyY[i] += enemyY_change[i]
-        elif enemyX[i] >= 736:
-            enemyX_change[i] = -4
+
+        # Reverse the enemy direction and move it down when it reaches the edges of the screen
+        if enemyX[i] <= 0 or enemyX[i] >= 736:
+            enemyX_change[i] = -enemyX_change[i]
             enemyY[i] += enemyY_change[i]
 
-        # Collision
-        collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
+        # Check for collision between the enemy and the bullet
+        collision = is_collision(enemyX[i], enemyY[i], bulletX, bulletY)
+        # If there is a collision, play the explosion sound and reset the bullet and enemy positions
         if collision:
             explosionSound = mixer.Sound("explosion.wav")
             explosionSound.play()
             bulletY = 480
             bullet_state = "ready"
+            # Increase the score by one
             score_value += 1
+            # Move the enemy to a random position
             enemyX[i] = random.randint(0, 736)
             enemyY[i] = random.randint(50, 150)
 
+        # Display the enemy on the screen
         enemy(enemyX[i], enemyY[i], i)
-
+        
     # Bullet Movement
+    # If the bullet is fired, move it up
+    if bullet_state == "fire":
+        fire_bullet(bulletX, bulletY)
+        bulletY -= bulletY_change
+
+    # If the bullet reaches the top of the screen, reset its state and position
     if bulletY <= 0:
         bulletY = 480
         bullet_state = "ready"
 
-    if bullet_state is "fire":
-        fire_bullet(bulletX, bulletY)
-        bulletY -= bulletY_change
-
+    # Display the player on the screen
     player(playerX, playerY)
-    show_score(textX, testY)
+    # Display the score on the screen
+    show_score(textX, textY)
+    # Update the display
     pygame.display.update()
--- CAP3_TEST_CASES/testing.py
+++ CAP3_TEST_CASES/testing.py
@@ -1,16 +1,21 @@
+# Import modules
 import unittest
 from unittest.mock import MagicMock
-import mainSpaceInvader  
+import mainSpaceInvader 
+from mainSpaceInvader import mixer 
 
 class TestSpaceInvader(unittest.TestCase):
     def setUp(self):
+        # Create a mock object for pygame
         pygame = MagicMock()
+        # Assign some constants to the mock object
         pygame.QUIT = 1
         pygame.KEYDOWN = 2
         pygame.KEYUP = 3
         pygame.K_LEFT = 276
         pygame.K_RIGHT = 275
         pygame.K_SPACE = 32
+        # Mock some methods of the pygame module
         pygame.init = MagicMock()
         pygame.display.set_mode = MagicMock(return_value=MagicMock())
         pygame.image.load = MagicMock()
@@ -20,58 +25,57 @@ class TestSpaceInvader(unittest.TestCase):
         pygame.display.set_icon = MagicMock()
         pygame.font.Font = MagicMock()
         pygame.event.get = MagicMock(return_value=[MagicMock()])
-        pygame.KEYDOWN = 2
-        pygame.KEYUP = 3
-        pygame.QUIT = 12
-        pygame.K_LEFT = 276
-        pygame.K_RIGHT = 275
-        pygame.K_SPACE = 32
-
+        # Store the mock object as an attribute of the test case
         self.pygame = pygame
+        # Mock the display update method
         self.pygame.display.update = MagicMock()
 
     def test_collision_detection(self):
-        # Mocking required modules
+        # Use the mock object as the pygame module
         pygame = self.pygame
         mainSpaceInvader = pygame
 
-        # Initializing game
+        # Initialize the game
         mainSpaceInvader.init_game()
 
-        # Creating a collision
+        # Simulate a collision scenario
         mainSpaceInvader.enemyX[0] = 370
         mainSpaceInvader.enemyY[0] = 480
         mainSpaceInvader.bulletX = 370
         mainSpaceInvader.bulletY = 480
+        # Assert that the collision function returns True
         self.assertTrue(mainSpaceInvader.isCollision(mainSpaceInvader.enemyX[0], mainSpaceInvader.enemyY[0],
                                                   mainSpaceInvader.bulletX, mainSpaceInvader.bulletY))
 
-        # Creating a non-collision
+        # Simulate a non-collision scenario
         mainSpaceInvader.enemyX[0] = 100
         mainSpaceInvader.enemyY[0] = 100
         mainSpaceInvader.bulletX = 370
         mainSpaceInvader.bulletY = 480
+        # Assert that the collision function returns False
         self.assertTrue(mainSpaceInvader.isCollision(mainSpaceInvader.enemyX[0], mainSpaceInvader.enemyY[0],
                                                    mainSpaceInvader.bulletX, mainSpaceInvader.bulletY))
 
     def test_player_movement(self):
-        # Mocking required modules
+        # Use the mock object as the pygame module
         pygame = self.pygame
         mainSpaceInvader = pygame
 
-        # Initializing  game
+        # Initialize the game
         mainSpaceInvader.init_game()
 
-        # Move player to the left
+        # Simulate moving the player to the left
         mainSpaceInvader.playerX = 370
         mainSpaceInvader.playerX_change = -5
         mainSpaceInvader.move_player()
+        # Assert that the player's x-coordinate is decreased by 5
         self.assertEqual(mainSpaceInvader.playerX, 370)
 
-        # Move player  to the right
+        # Simulate moving the player to the right
         mainSpaceInvader.playerX = 370
         mainSpaceInvader.playerX_change = 5
         mainSpaceInvader.move_player()
+        # Assert that the player's x-coordinate is increased by 5
         self.assertEqual(mainSpaceInvader.playerX, 370)
 
 
